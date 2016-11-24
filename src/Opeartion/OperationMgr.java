b0ff4b68e61b62658f628501cfb2b1e3a161835d
JarvisZhangUTA <zhangjw.uta@gmail.com>
2016-11-23 20:09:59 -0600

    Mgr

--- src/Opeartion/OperationMgr.java
+++ src/Opeartion/OperationMgr.java
@@ -22,24 +22,39 @@ public class OperationMgr {
 		operation = new OpeartionImpl();
 	}
 	
-	public boolean addComponent(CompositeComponent parent,Phrase phrase, int index){
-		
-		return true;
+	public boolean addComponent(RequirementComponent parent,RequirementComponent child,int index){
+		 return operation.addComponent(parent, child, index);
 	}
 	
-	public List<RequirementComponent> getComponent(){
-		return null;
+	public boolean removeComponent(RequirementComponent child){
+		return operation.removeComponent(child);
 	}
 	
-	public boolean editComponent(){
-		return true;
+	public List<RequirementComponent> getChild(RequirementComponent parent){
+		return operation.getChild(parent);
+	}
+	
+	public RequirementComponent getComponent(String id){
+		return operation.getComponent(id);
+	}
+	
+	public boolean editComponent(RequirementComponent oldCom,RequirementComponent newCom,int newIndex){
+		return operation.editComponent(oldCom, newCom, newIndex);
 	}
 	
 	public String generateRequirement(){
-		return "";
+		return operation.generateRequirement();
 	}
 	
-	public boolean exportRequirement(){
-		return true;
+	public boolean exportRequirement(String path){
+		return operation.exportRequirement(path);
+	}
+	
+	public boolean undo(){
+		return operation.undo();
+	}
+	
+	public boolean redo(){
+		return operation.redo();
 	}
 }
