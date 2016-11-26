0f3f3774e9309931e9e2de4520306fbaccb7ae61
tusha <tusha@DESKTOP-0LHUPH6>
2016-11-25 20:08:27 -0600

    Edit BP ans step

--- src/Controller/EditBusinessProcessesController.java
+++ src/Controller/EditBusinessProcessesController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import compoiste.CompositeComponent;
 import compoiste.Phrase;
 
 import BusinessObjects.Action;
@@ -9,59 +10,60 @@ import BusinessObjects.RequirementComponent;
 import BusinessObjects.Step;
 import Commands.EditComponent;
 import Commands.RemoveComponent;
+import Opeartion.OperationMgr;
 
 public class EditBusinessProcessesController {
-	
+
 	public void removeRequirementComponent(RequirementComponent requirementComponent)
 	{
-		RemoveComponent remove=new RemoveComponent(requirementComponent);
-		remove.execute();
-		
+		OperationMgr opManager = OperationMgr.getInstance();
+		opManager.removeComponent(requirementComponent);
 	}
-	
-	public void editBusinessProcess(BusinessProcess oldBp,String verb, String noun, String sentence, int position){
-        
-        Phrase phrase = new Phrase(verb, noun);
+
+	public void editBusinessProcess(String oldBpId,String verb, String noun, String sentence, int position){
+
+		Phrase phrase = new Phrase(verb, noun);
+
+		if(sentence!=null && sentence.length()>0)
+			phrase.setSentence(sentence);		
+
+		CompositeComponent newBpComponent = new CompositeComponent(phrase);
+		OperationMgr opManager = OperationMgr.getInstance();
+		CompositeComponent oldBpComponent = (CompositeComponent) opManager.getComponent(oldBpId);
+		newBpComponent.setChild(oldBpComponent.getChild());
+		opManager.editComponent(oldBpComponent, newBpComponent, position);
+
+	}
+
+	public void editStep(String oldStepId, String parentId, String verb, String noun, String sentence, int position){
 		
+		Phrase phrase = new Phrase(verb, noun);
+
 		if(sentence!=null && sentence.length()>0)
 			phrase.setSentence(sentence);
 		
-		BusinessProcess newBp = new BusinessProcess(phrase);
-		newBp.setStepsList(oldBp.getStepsList());
+		CompositeComponent newStepComponent = new CompositeComponent(phrase);		
+		OperationMgr opManager = OperationMgr.getInstance();
+		CompositeComponent oldStepComponent = (CompositeComponent) opManager.getComponent(oldStepId);
+		CompositeComponent parent = (CompositeComponent) opManager.getComponent(parentId);
+		newStepComponent.setChild(oldStepComponent.getChild());
+		newStepComponent.setParent(parent);
+		opManager.editComponent(oldStepComponent, newStepComponent, position);
 
-		EditComponent edit = new EditComponent(oldBp,newBp,position);
-		edit.execute();
-		
 	}
-	
-	public void editStep(Step oldStep, BusinessProcess parent, String verb, String noun, String sentence, int position){
-		
-		 Phrase phrase = new Phrase(verb, noun);
-			
-			if(sentence!=null && sentence.length()>0)
-				phrase.setSentence(sentence);
-			
-			Step newStep = new Step(phrase);
-			
-			newStep.setActionsList(oldStep.getActionsList());
-			newStep.setParent(parent);
-			
-			EditComponent edit = new EditComponent(oldStep,newStep,position);
-			edit.execute();
-			
-	}
-	
+
 	public void editAction(Action oldAction, Step parent, String verb, String noun, String sentence, int position){
-		 Phrase phrase = new Phrase(verb, noun);
-			
-			if(sentence!=null && sentence.length()>0)
-				phrase.setSentence(sentence);
-			
-			Action newAction = new Action(phrase);
-			
-			newAction.setParent(parent);
-			
-			EditComponent edit = new EditComponent(oldAction,newAction,position);
-			edit.execute();
+
+		Phrase phrase = new Phrase(verb, noun);
+
+		if(sentence!=null && sentence.length()>0)
+			phrase.setSentence(sentence);
+
+		Action newAction = new Action(phrase);
+
+		newAction.setParent(parent);
+
+		EditComponent edit = new EditComponent(oldAction,newAction,position);
+		edit.execute();
 	}
 }
