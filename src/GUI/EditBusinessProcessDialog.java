439a66b84e4e541d5a855c8ca347992c01c172e4
tusha <tusha@DESKTOP-0LHUPH6>
2016-11-25 20:09:27 -0600

    Edit BP

--- src/GUI/EditBusinessProcessDialog.java
+++ src/GUI/EditBusinessProcessDialog.java
@@ -32,90 +32,85 @@ public class EditBusinessProcessDialog extends JDialog {
 	private JLabel lblSequenceNumber;
 	private JComboBox cbSequenceNumber;
 
-	
+
 
 	/**
 	 * Create the dialog.
 	 */
-	public EditBusinessProcessDialog(RETGUI parent,BusinessProcess bp) {
+	public EditBusinessProcessDialog(RETGUI parent, String bpId) {
+
+
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(null);
 		{
 			{
-				
+
 				{
-					txtVerb = new JTextField(bp.getPhrase().getVerb());
+					txtVerb = new JTextField();
 					txtVerb.setBounds(171, 44, 86, 22);
 					getContentPane().add(txtVerb);
 					lblVerb = new JLabel("Verb : ");
 					lblVerb.setBounds(104, 41, 71, 28);
 					getContentPane().add(lblVerb);
-					
-					txtNoun = new JTextField(bp.getPhrase().getNoun());
+
+					txtNoun = new JTextField();
 					txtNoun.setBounds(171, 79, 86, 22);
 					getContentPane().add(txtNoun);
 					lblNoun = new JLabel("Noun : ");
 					lblNoun.setBounds(104, 82, 64, 22);
 					getContentPane().add(lblNoun);
-					txtSentance = new JTextField(bp.getPhrase().getSentence());
+					txtSentance = new JTextField();
 					txtSentance.setBounds(171, 114, 216, 22);
 					getContentPane().add(txtSentance);
-					
+
 					lblSentance=new JLabel("Sentence : ");
 					lblSentance.setBounds(104, 114, 71, 22);
 					getContentPane().add(lblSentance);
-					
+
 					JComboBox cbSequenceNumber = new JComboBox();
 					cbSequenceNumber.setBounds(171, 146, 86, 22);
 					getContentPane().add(cbSequenceNumber);
-					
-					JLabel lblNewLabel = new JLabel("Sequence No : ");
-					lblNewLabel.setBounds(104, 149, 56, 16);
-					getContentPane().add(lblNewLabel);
-				
-						JButton saveButton = new JButton("Save");
-						saveButton.addActionListener(new ActionListener() {
-							public void actionPerformed(ActionEvent arg0) {
-							}
-						});
-						saveButton.setBounds(164, 201, 93, 22);
-						getContentPane().add(saveButton);
-						saveButton.setActionCommand("OK");
-						getRootPane().setDefaultButton(saveButton);
-					
-					
 
-					lblSequenceNumber = new JLabel("Sequence Number : ");
-				
-					
+					lblSequenceNumber = new JLabel("Sequence No : ");
+					lblSequenceNumber.setBounds(104, 149, 56, 16);
+					getContentPane().add(lblSequenceNumber);
+
+					JButton saveButton = new JButton("Save");
+					saveButton.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent arg0) {
+						}
+					});
+					saveButton.setBounds(164, 201, 93, 22);
+					getContentPane().add(saveButton);
+					saveButton.setActionCommand("OK");
+					getRootPane().setDefaultButton(saveButton);					
+
 					DefineBusinessProcessController dbpController= new DefineBusinessProcessController();
-					int numberOfBusinessProcess=dbpController.getBusinessProcesses().size();
 					
+					String[] phraseInfo = dbpController.getComponentPhraseInfo(bpId);
+					txtVerb.setText(phraseInfo[0]);
+					txtNoun.setText(phraseInfo[1]);
+					txtSentance.setText(phraseInfo[2]);		
+
+					int numberOfBusinessProcess=dbpController.getBusinessProcesses().size();										
 					for(int i = 1; i <=( numberOfBusinessProcess); i++)
 						cbSequenceNumber.addItem(i);
 					
-					int tempBp=Repository.getInstance().getBusinessProcessList().indexOf(bp);
-					System.out.println("currently selected" + bp + " "+ tempBp);
-					
-					//TODO
-					cbSequenceNumber.setSelectedIndex(tempBp);
-			
-					
+					int bpIndex = Integer.parseInt(bpId.split(".")[0]);
+					cbSequenceNumber.setSelectedIndex(bpIndex);													
+
 					saveButton.addActionListener(new ActionListener() {
 						public void actionPerformed(ActionEvent arg) {
-							
+
 							EditBusinessProcessesController editBPController=new EditBusinessProcessesController();
-							System.out.println( "new selected Index "+cbSequenceNumber.getSelectedIndex());
-							editBPController.editBusinessProcess(bp, txtVerb.getText(), txtNoun.getText(), txtSentance.getText(), cbSequenceNumber.getSelectedIndex());
-							
+							editBPController.editBusinessProcess(bpId, txtVerb.getText(), txtNoun.getText(), txtSentance.getText(), cbSequenceNumber.getSelectedIndex());
+
 							parent.refreshTree();
-							
+
 							dispose();
-							
+
 						}
-					});
-					
-					
+					});								
 				}
 			}
 		}
