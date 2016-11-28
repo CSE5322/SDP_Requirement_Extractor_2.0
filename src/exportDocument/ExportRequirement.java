2de45abd50deb16028392d7b7708576834f203e9
abhishekdhotre <abhidhotre91@yahoo.com>
2016-11-27 22:49:54 -0600

    Update ExportRequirement.java

--- src/exportDocument/ExportRequirement.java
+++ src/exportDocument/ExportRequirement.java
@@ -1,24 +1,27 @@
 package exportDocument;
 
 public class ExportRequirement {
-	String requirementData;
-	String filePath;
+	static String  requirementData;
+	static String filePath;
+	
 	public void ExportData(String requirement, String path, String extension){
 		requirementData = requirement;
 		filePath = path;
-		switch(extension){
-		case ("pdf"):{
+		if(extension.equals("pdf")){
 			ExportPdf pdf = new ExportPdf();
 			pdf.ExportDataPdf();
 		}
-		case ("txt"):{
-			ExportTxt txt = new ExportTxt();
-			txt.ExportDataTxt();
-		}
-		case ("doc"):{
+		else if (extension.equals("doc")){
 			ExportDoc doc = new ExportDoc();
 			doc.ExportDataDoc();
 		}
+		else if (extension.equals("txt")){
+			ExportTxt txt = new ExportTxt();
+			txt.ExportDataTxt();
+		}
+		else if (extension.equals("xml")){
+			ExportXML xml = new ExportXML();
+			xml.ExportDataXML();
 		}
 	};
 }
